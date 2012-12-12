ef8cf9fddfb7518a1c42521e22a7f7d8c8cc35e6
Hudson Ferreira <silvaferreira.hsf@gmail.com>
2012-12-12 16:19:58 -0200

    adicionando metodo p gerar a classe factory utils e removendo um espaco do metodo create events interface

---
+++ src/FactoryUtils.java
@@ -0,0 +1,191 @@
+package org.yakindu.sct.ui.editor.factories;
+
+import org.eclipse.draw2d.ColorConstants;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.gmf.runtime.diagram.core.preferences.PreferencesHint;
+import org.eclipse.gmf.runtime.diagram.core.services.ViewService;
+import org.eclipse.gmf.runtime.diagram.core.util.ViewUtil;
+import org.eclipse.gmf.runtime.draw2d.ui.figures.FigureUtilities;
+import org.eclipse.gmf.runtime.notation.Bounds;
+import org.eclipse.gmf.runtime.notation.DecorationNode;
+import org.eclipse.gmf.runtime.notation.Diagram;
+import org.eclipse.gmf.runtime.notation.Node;
+import org.eclipse.gmf.runtime.notation.NotationFactory;
+import org.eclipse.gmf.runtime.notation.ShapeStyle;
+import org.eclipse.gmf.runtime.notation.View;
+import org.yakindu.sct.model.sgraph.Entry;
+import org.yakindu.sct.model.sgraph.EntryKind;
+import org.yakindu.sct.model.sgraph.Region;
+import org.yakindu.sct.model.sgraph.SGraphFactory;
+import org.yakindu.sct.model.sgraph.State;
+import org.yakindu.sct.model.sgraph.Statechart;
+import org.yakindu.sct.model.sgraph.Transition;
+import org.yakindu.sct.ui.editor.DiagramActivator;
+import org.yakindu.sct.ui.editor.editor.StatechartDiagramEditor;
+import org.yakindu.sct.ui.editor.providers.SemanticHints;
+
+public final class FactoryUtils {
+
+    private static final int INITIAL_REGION_WIDTH = 400;
+    private static final int INITIAL_REGION_HEIGHT = 400;
+    private static final String INITIAL_REGION_NAME = "main region";
+
+    private static final int INITIAL_TEXT_COMPARTMENT_X = 10;
+    private static final int INITIAL_TEXT_COMPARTMENT_Y = 10;
+    private static final int INITIAL_TEXT_COMPARTMENT_HEIGHT = 400;
+    private static final int INITIAL_TEXT_COMPARTMENT_WIDTH = 200;
+
+    private static final int SPACING = 10;
+
+    private FactoryUtils() {
+    }
+
+    @SuppressWarnings("unchecked")
+    public static Node createLabel(View owner, String hint) {
+        DecorationNode nameLabel = NotationFactory.eINSTANCE
+                .createDecorationNode();
+        nameLabel.setType(hint);
+
+        ShapeStyle style = NotationFactory.eINSTANCE.createShapeStyle();
+        style.setFontColor(FigureUtilities.RGBToInteger(ColorConstants.black
+                .getRGB()));
+        nameLabel.getStyles().add(style);
+
+        ViewUtil.insertChildView(owner, nameLabel, ViewUtil.APPEND, true);
+        nameLabel.setLayoutConstraint(NotationFactory.eINSTANCE
+                .createLocation());
+        return nameLabel;
+    }
+
+    public static void createStatechartModel(Resource resource) {
+        createStatechartModel(resource,
+                DiagramActivator.DIAGRAM_PREFERENCES_HINT);
+    }
+
+    public static void createStatechartModel(Resource resource,
+            PreferencesHint preferencesHint) {
+        Statechart statechart = SGraphFactory.eINSTANCE.createStatechart();
+statechart.setSpecification("
+
+interface light:
+var off:boolean
+var on:boolean
+
+interface thermostat:
+var minimum:boolean
+var maximum:boolean
+
+interface:
+in event opendoor
+in event closedoor");
+
+    String lastSegment = resource.getURI().lastSegment();
+        String statechartName = lastSegment.substring(0,
+                lastSegment.indexOf('.'));
+        statechart.setName(statechartName);
+
+        Diagram diagram = ViewService.createDiagram(statechart,
+                StatechartDiagramEditor.ID, preferencesHint);
+        diagram.setElement(statechart);
+        // Add to resource
+        resource.getContents().add(statechart);
+        resource.getContents().add(diagram);
+        // Create an initial region
+        Region region = SGraphFactory.eINSTANCE.createRegion();
+        region.setName(INITIAL_REGION_NAME);
+        statechart.getRegions().add(region);
+        Node regionView = ViewService.createNode(diagram, region,
+                SemanticHints.REGION, preferencesHint);
+        setRegionViewLayoutConstraint(regionView);
+        // // Create an initial state
+        Entry initialState = SGraphFactory.eINSTANCE.createEntry();
+        initialState.setKind(EntryKind.INITIAL);
+        region.getVertices().add(initialState);
+        Node initialStateView = ViewService.createNode(
+                getRegionCompartmentView(regionView), initialState,
+                SemanticHints.ENTRY, preferencesHint);
+        setInitialStateViewLayoutConstraint(initialStateView);
+        
+        
+State refrigeratordoorclosed = SGraphFactory.eINSTANCE.createState();
+ refrigeratordoorclosed.setName("refrigeratordoorclosed"); 
+refrigeratordoorclosed.setSpecification("entry/
+light.off = true;
+thermostat.minimum = true;
+light.on = false;
+thermostat.maximum = false"); 
+region.getVertices().add(refrigeratordoorclosed); 
+Node refrigeratordoorclosedNode = ViewService.createNode(
+getRegionCompartmentView(regionView), refrigeratordoorclosed,
+SemanticHints.STATE, preferencesHint);
+setStateViewLayoutConstraint(refrigeratordoorclosedNode);
+
+State dooropened = SGraphFactory.eINSTANCE.createState();
+ dooropened.setName("dooropened"); 
+dooropened.setSpecification("entry/
+light.off = true;
+thermostat.minimum = true;
+light.on = false;
+thermostat.maximum = false"); 
+region.getVertices().add(dooropened); 
+Node dooropenedNode = ViewService.createNode(
+getRegionCompartmentView(regionView), dooropened,
+SemanticHints.STATE, preferencesHint);
+setStateViewLayoutConstraint(dooropenedNode);
+
+Transition opendoor = SGraphFactory.eINSTANCE.createTransition();
+opendoor.setSpecification("opendoor");
+ opendoor.setSource(refrigeratordoorclosed);
+opendoor.setTarget(dooropened);
+Transition closedoor = SGraphFactory.eINSTANCE.createTransition();
+closedoor.setSpecification("closedoor");
+ closedoor.setSource(dooropened);
+closedoor.setTarget(refrigeratordoorclosed);
+Transition transition = SGraphFactory.eINSTANCE.createTransition();
+transition.setSource(initialState);
+transition.setTarget(refrigeratordoorclosed);
+initialState.getOutgoingTransitions().add(transition);
+ViewService.createEdge(initialStateView, refrigeratordoorclosedNode, transition,
+SemanticHints.TRANSITION, preferencesHint);Node textCompartment = ViewService.createNode(diagram, statechart,
+                SemanticHints.STATECHART_TEXT, preferencesHint);
+        setTextCompartmentLayoutConstraint(textCompartment);
+    }
+
+    private static void setStateViewLayoutConstraint(Node stateNode) {
+        Bounds bounds = NotationFactory.eINSTANCE.createBounds();
+        bounds.setX(40);
+        bounds.setY(80);
+        stateNode.setLayoutConstraint(bounds);
+    }
+
+    private static void setInitialStateViewLayoutConstraint(
+            Node initialStateView) {
+        Bounds bounds = NotationFactory.eINSTANCE.createBounds();
+        bounds.setX(70);
+        bounds.setY(20);
+        initialStateView.setLayoutConstraint(bounds);
+    }
+
+    private static View getRegionCompartmentView(View regionView) {
+        return (View) regionView.getChildren().get(1);
+    }
+
+    private static void setTextCompartmentLayoutConstraint(Node textCompartment) {
+        Bounds bounds = NotationFactory.eINSTANCE.createBounds();
+        bounds.setX(INITIAL_TEXT_COMPARTMENT_X);
+        bounds.setY(INITIAL_TEXT_COMPARTMENT_Y);
+        bounds.setHeight(INITIAL_TEXT_COMPARTMENT_HEIGHT);
+        bounds.setWidth(INITIAL_TEXT_COMPARTMENT_WIDTH);
+        textCompartment.setLayoutConstraint(bounds);
+    }
+
+    private static void setRegionViewLayoutConstraint(Node regionView) {
+    Bounds bounds = NotationFactory.eINSTANCE.createBounds();
+    bounds.setX(INITIAL_TEXT_COMPARTMENT_WIDTH + INITIAL_TEXT_COMPARTMENT_X
+                + SPACING);
+    bounds.setY(INITIAL_TEXT_COMPARTMENT_Y);
+    bounds.setHeight(INITIAL_REGION_HEIGHT);
+    bounds.setWidth(INITIAL_REGION_WIDTH);
+    regionView.setLayoutConstraint(bounds);
+    }
+}
