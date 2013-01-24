db6cb513725c05b76f9c0286dc194ee296c6100a
Hudson Ferreira <silvaferreira.hsf@gmail.com>
2013-01-24 16:34:34 -0200

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+version = '0.0.1'
+readme = open('README.rst').read()
+
+setup(name='yakinduparser',
+      version=version,
+      description="A parser that identifies notes at a .odt file, based on Finite-State Machine.",
+      long_description=readme,
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='parser odt fsm python',
+      authors='Hudson Ferreira', "Eduardo Carvalho"
+      authors_email='silvaferreira.hsf@gmail.com', 'eduardo.oak80@gmail.com'
+      url='',
+      license='',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        "should_dsl",
+        "lxml",
+        "specloud",
+        "nltk",
+        "python-magic"
+        ],
+      )
