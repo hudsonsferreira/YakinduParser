cb66ff62a756e1f8d1c80e0cdfc34632a350eb63
Hudson Ferreira <silvaferreira.hsf@gmail.com>
2013-03-18 20:42:12 -0300

    inheritance

--- generator/spec/spec_generator_spec.py
+++ generator/spec/spec_generator_spec.py
@@ -1,16 +1,21 @@
+import sys
+sys.path.append("/home/hudson/projetos/yakinduparser/generator")
+
+
 from unittest import TestCase
 from should_dsl import should, should_not
-from generator.spec_generator import SpecGenerator
+from spec_generator import SpecGenerator
 
 
 class SpecGenerator_Spec(TestCase):
 
     def setUp(self):
-        self.spec_generator = SpecGenerator()
+        self.spec_generator = SpecGenerator('/home/hudson/projetos/yakinduparser/parser/spec/resources/refrigerator.odt')
 
     def test_if_there_are_at_least_two_states(self):
         states_list = []
         states_list = self.spec_generator._get_states_content()
+        states_list |should_not| be_empty
         states_list |should| be_greater_than_or_equal_to(2)
 
     def test_if_there_are_a_initial_state(self):
--- generator/spec_generator.py
+++ generator/spec_generator.py
@@ -1,6 +1,14 @@
-from parser.yakindu_parser import YakinduParser
+import sys
+
+sys.path.append("/home/hudson/projetos/yakinduparser/parser")
+
+from yakindu_parser import YakinduParser
 
 class SpecGenerator(YakinduParser):
 
-    def __init__(self):
-        pass
+    def __init__(self, path):
+       YakinduParser.__init__(self, path)
+
+    def take_states(self):
+        states_list = []
+        states_list = self._get_states_content()
