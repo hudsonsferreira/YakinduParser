b5089ce31857d1bbc545b5f8db4c18be2e02921b
Hudson Ferreira <silvaferreira.hsf@gmail.com>
2013-03-19 14:48:18 -0300

    created states test interface

--- generator/spec/spec_generator_spec.py
+++ generator/spec/spec_generator_spec.py
@@ -12,14 +12,32 @@ class SpecGenerator_Spec(TestCase):
     def setUp(self):
         self.spec_generator = SpecGenerator('/home/hudson/projetos/yakinduparser/parser/spec/resources/refrigerator.odt')
 
-    def test_if_there_are_at_least_two_states(self):
+    def test_there_are_at_least_two_states(self):
         states_list = []
         states_list = self.spec_generator._get_states_content()
         states_list |should_not| be_empty
         states_list |should| be_greater_than_or_equal_to(2)
+        states_list |should| equal_to(['refrigeratordoorclosed', 'refrigeratordooropened'])
 
-    def test_if_there_are_a_initial_state(self):
+    def test_there_is_a_initial_state(self):
         initial_state_list = []
         initial_state_list = self.spec_generator._get_initial_state()
         initial_state_list |should_not| be_empty
         initial_state_list |should| be_greater_than_or_equal_to(1)
+        initial_state_list |should| equal_to([['refrigerator', 'door', 'closed'],
+                                              ['refrigerator', 'door', 'closed']])
+
+    def test_sequence_transitions(self):
+        sequence_transitions_list = []
+        sequence_transitions_list = self.spec_generator._join_sequence_transitions()
+        sequence_transitions_list |should_not| be_empty
+        sequence_transitions_list |should| be_greater_than_or_equal_to(1)
+        sequence_transitions_list |should| equal_to([['refrigeratordoorclosed', 'opendoor', 'refrigeratordooropened'],
+                                                     ['refrigeratordooropened', 'closedoor', 'refrigeratordoorclosed']])
+
+    def test_states_test_interface(self):
+        self.spec_generator.create_states_test_interface() |should| equal_to("states_list = []\n"+\
+        "states_list = self.spec_generator._get_states_content()\n"+\
+        "states_list |should_not| be_empty\n"+\
+        "states_list |should| be_greater_than_or_equal_to(2)\n"+\
+        "states_list |should| equal_to(['refrigeratordoorclosed', 'refrigeratordooropened'])")
--- generator/spec_generator.py
+++ generator/spec_generator.py
@@ -1,5 +1,4 @@
 import sys
-
 sys.path.append("/home/hudson/projetos/yakinduparser/parser")
 
 from yakindu_parser import YakinduParser
@@ -8,7 +7,17 @@ class SpecGenerator(YakinduParser):
 
     def __init__(self, path):
        YakinduParser.__init__(self, path)
+       self._indentation = 4 * ' '
 
-    def take_states(self):
+    def create_states_test_interface(self):
+        pharse = "states_list = []\n"+\
+        "states_list = self.spec_generator._get_states_content()\n"+\
+        "states_list |should_not| be_empty\n"+\
+        "states_list |should| be_greater_than_or_equal_to(%d)\n"+\
+        "states_list |should| equal_to(['%s', '%s'])"
+        
         states_list = []
         states_list = self._get_states_content()
+        length = len(states_list)
+        final_pharse = pharse %(length, states_list[length-2], states_list[length-1])
+        return final_pharse
